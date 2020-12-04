2d7e458bb6527b2e18867bf3224b8c494604a325
Max <maxim.macari.dam@gmail.com>
2020-12-04 15:24:54 +0100

    Initial Commit

---
+++ ParallaxCarousel/ParallaxCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxCarouselApp.swift
+//  ParallaxCarousel
+//
+//  Created by Maxim Macari on 4/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
